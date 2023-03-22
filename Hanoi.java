e22974ff3357b2d597903d6b37647213cd895e41
Ritisna Priyadarshini <116563485+Ritisna@users.noreply.github.com>
2023-03-22 11:14:49 +0530

    Add files via upload

---
+++ Hanoi.java
@@ -0,0 +1,16 @@
+class Hanoi {
+    public static void towerOfHanoi(int num, char src, char dst, char temp) {
+        if (num < 1) {
+        return;
+        }
+        towerOfHanoi(num - 1, src, temp, dst);
+        System.out.println("Move " + num + " disk from peg " + src + " to peg " +
+        dst);
+        towerOfHanoi(num - 1, temp, dst, src);
+        }
+        public static void main(String[] args) {
+        int num = 4;
+        System.out.println("The sequence of moves are :\n");
+        towerOfHanoi(num, 'A', 'C', 'B');
+        }
+}
